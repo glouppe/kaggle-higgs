2a7d1d7c859872a790701f30a76e1afd0bbde5de
Gilles Louppe <g.louppe@gmail.com>
2014-06-17 14:04:40 +0200

    cleanup

--- glouppe/grid.py
+++ glouppe/grid.py
@@ -17,14 +17,14 @@ X, y, w, _ = load_train()
 # Look for the best model
 print "Optimize parameters in 5-CV..."
 
-#prefix = "gbrt"
-#from sklearn.ensemble import GradientBoostingClassifier
-#Classifier = GradientBoostingClassifier
-#grid = ParameterGrid({"n_estimators": [500],
-#                      "learning_rate": [0.1],
-#                      "max_depth": [6],
-#                      "max_features": [None],
-#                      "min_samples_leaf": [1]})
+prefix = "gbrt-b"
+from sklearn.ensemble import GradientBoostingClassifier
+Classifier = GradientBoostingClassifier
+grid = ParameterGrid({"n_estimators": [500],
+                      "learning_rate": [0.1],
+                      "max_depth": [4, 5],
+                      "max_features": [23, 25, 27],
+                      "min_samples_leaf": [100, 125]})
 
 #prefix = "xgb"
 #from xg import XGBoostClassifier
@@ -48,11 +48,11 @@ print "Optimize parameters in 5-CV..."
 #                      "max_features": [10, 15, 20, 25, 30],
 #                      "n_jobs": [24]})
 
-from sklearn.preprocessing import StandardScaler
-tf = StandardScaler()
-X = X.astype(np.float32)
-tf.fit(X)
-X = tf.transform(X)
+#from sklearn.preprocessing import StandardScaler
+#tf = StandardScaler()
+#X = X.astype(np.float32)
+#tf.fit(X)
+#X = tf.transform(X)
 
 #prefix = "sgd"
 #from sklearn.ensemble import BaggingClassifier
@@ -66,19 +66,19 @@ X = tf.transform(X)
 #Classifier = KNeighborsClassifier
 #grid = ParameterGrid({"n_neighbors": [5, 10, 20, 30, 40, 50]})
 
-prefix = "mlp"
+#prefix = "mlp"
 #prefix = "bagging-mlp"
-from sklearn.neural_network import MultilayerPerceptronClassifier
-Classifier = MultilayerPerceptronClassifier
-grid = ParameterGrid({"n_hidden": [[50,50,50], [75,75,75], [100,100,100]],
-                      "shuffle": [True], 
-                      "verbose": [False],
-                      "batch_size": [100],
-                      "algorithm": ["sgd"],
-                      "max_iter": [200],
-                      "eta0": [0.0075, 0.01, 0.025],
-                      "learning_rate": ["constant"],
-                      "alpha": [0.00001]})
+#from sklearn.neural_network import MultilayerPerceptronClassifier
+#Classifier = MultilayerPerceptronClassifier
+#grid = ParameterGrid({"n_hidden": [[50,50,50], [75,75,75], [100,100,100]],
+#                      "shuffle": [True], 
+#                      "verbose": [False],
+#                      "batch_size": [100],
+#                      "algorithm": ["sgd"],
+#                      "max_iter": [200],
+#                      "eta0": [0.0075, 0.01, 0.025],
+#                      "learning_rate": ["constant"],
+#                      "alpha": [0.00001]})
 #from sklearn.ensemble import BaggingClassifier
 #Classifier = partial(BaggingClassifier, base_estimator=MultilayerPerceptronClassifier(n_hidden=50, shuffle=True))
 #grid = ParameterGrid({"n_estimators": [20], "n_jobs": [20], "bootstrap": [False, True], "max_features": [25, 27, 30]})
@@ -161,12 +161,7 @@ except:
 print "Save test predictions for stacking..."
 
 X_test, _, _, ids = load_test()
-X_test = tf.transform(X_test.astype(np.float32))
-
-try:
-    d = -clf.decision_function(X_test)
-except:
-    d = clf.predict_proba(X_test)[:, 0]
-
+#X_test = tf.transform(X_test.astype(np.float32))
+d = clf.predict_proba(X_test)[:, 0]
 d = d.flatten()
 np.save("stack/%s-test.npy" % prefix, d)
